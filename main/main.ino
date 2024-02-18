7642316c3cf90fca65612893724641b15aa068ab
Matt740 <matthew.pihowich@mail.utoronto.ca>
2024-02-17 21:21:56 -0500

    defining functions
    function definition

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,42 @@
 void setup() {
 // put your setup code here, to run once:
-float g = 9.81;
-float m_arm = .04;
-float h_cm = 0.0707;
-float I_am
+  float g = 9.81;
+  float m_arm = .04;
+  float h_cm = 0.0707;
+  float I_am
 
 
 
 }
 
+void secure_catapult_arm () {
+
+
+
+}
+
+void position_base () {
+
+
+}
 
 
 int get_arm_angle() {
 
 
 }
+
+void position_arm() {
+
+
+}
+
+void release_arm () {
+
+
+}
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
