00b482645f48e1ec0575cceb7ca8ad0a6c10f092
Matt740 <matthew.pihowich@mail.utoronto.ca>
2024-02-17 21:17:09 -0500

    Create main.ino
    initializing variable

---
+++ main/main.ino
@@ -0,0 +1,22 @@
+void setup() {
+// put your setup code here, to run once:
+float g = 9.81;
+float m_arm = .04;
+float h_cm = 0.0707;
+float I_am
+
+
+
+}
+
+
+
+int get_arm_angle() {
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
